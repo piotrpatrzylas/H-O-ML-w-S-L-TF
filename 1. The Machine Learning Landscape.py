12a0a187947899b5423a3201434e8475f9d397a3
Piotr Patrzylas <piotrpatrzylas@gmail.com>
2020-08-02 19:15:10 +0100

    Chapter 1 solutions.

--- 1. The Machine Learning Landscape.py
+++ 1. The Machine Learning Landscape.py
@@ -8,4 +8,20 @@ It is teaching computer to learn without explicitly stating all the information/
 -problems for which inputs are changing rapidly, need to adapt/learn quickly
 -getting insight from Big Data
 
+3. What is a labeled training set?
+This is a set used in supervised ML for training an algorithm. It contains y variable (response).
+
+4. What are the two most common supervised tasks?
+Classification and regression.
+
+5. Can you name four common unsupervised tasks?
+-clustering
+-dimension reduction (PCA)
+-visualization (dendrograms)
+-anomaly detection
+
+6. What type of ML algorithm would you use to allow a robot to walk in various unknown terrains?
+Monte Carlo algorithm in Reinforced ML.
+
+7.
 """
