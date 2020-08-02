617b1e8435fdfdadc9ee5256162bcbda4cd01ce5
Piotr Patrzylas <piotrpatrzylas@gmail.com>
2020-08-02 18:41:32 +0100

    Chapter 1 solutions.

---
+++ 1. The Machine Learning Landscape.py
@@ -0,0 +1,11 @@
+"""
+1. How would you define Machine Learning?
+It is teaching computer to learn without explicitly stating all the information/rules about the World.
+
+2. Can you name four types of problems where it shines?
+-complicated problems thad would require lots of tuning/instructions
+-hard problems where no simple solution(s) exist
+-problems for which inputs are changing rapidly, need to adapt/learn quickly
+-getting insight from Big Data
+
+"""
