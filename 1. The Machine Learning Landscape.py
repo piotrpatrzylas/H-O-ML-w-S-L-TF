783cc4729428331682160f9b5dec8c9cb9639a37
Piotr Patrzylas <piotrpatrzylas@gmail.com>
2020-08-06 18:12:19 +0100

    Chapter 1 solutions.

--- 1. The Machine Learning Landscape.py
+++ 1. The Machine Learning Landscape.py
@@ -23,5 +23,20 @@ Classification and regression.
 6. What type of ML algorithm would you use to allow a robot to walk in various unknown terrains?
 Monte Carlo algorithm in Reinforced ML.
 
-7.
+7. What type of algorithm would you use to segment your customers into multiple groups?
+Some sort of Unsupervised ML clustering algorithm - like. hierarchical clustering.
+
+8. Would you frame the problem of spam detection as a supervised learning problem or an unsupervised learning problem.
+This would be (at least initially) supervised learning problem - we need to give an example/define what spam is and what
+spam is not. We could also try to use unsupervised ML - for example to cluster emails into 2 groups hoping that they
+will be divided into spam and not-spam, however results for this could far from ideal.
+
+9. What is an online learning system?
+Online learning system is system that is updated in small batches continuously
+
+10. What is out-of-core learning?
+This is a type of online learning system where data is too big to fit into computers memory - it must bu cut into chunks.
+
+11. What type of learning algorithm relies on a similarity measure to make prediction?
+Instance based learning algorithm.
 """
